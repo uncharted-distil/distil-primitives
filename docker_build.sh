e8a8aca4bc05c0fc3ee955d8b9c5841857fc8fa3
cbethune <cbethune@uncharted.software>
2019-04-12 16:53:24 -0400

    builds 2019.2.18 primitives image with distil primitives

---
+++ docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build . -t registry.datadrivendiscovery.org/uncharted/distil-integration/complete-uncharted:latest
---
+++ docker_push.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker push registry.datadrivendiscovery.org/uncharted/distil-integration/complete-uncharted:latest
