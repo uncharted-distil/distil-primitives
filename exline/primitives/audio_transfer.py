b0d089e11a3753d9934a06f29b390b5bd102dc93
Jewell Thomas <jewell.thomas@qntfy.com>
2019-05-15 22:42:34 -0400

    Revert "fix issue with indexes in audio pipeline"
    This reverts commit 8a2c3d8688bc6c7f29f306b012886ea141ce9d25.

--- exline/primitives/audio_transfer.py
+++ exline/primitives/audio_transfer.py
@@ -104,13 +104,12 @@ class AudioTransferPrimitive(unsupervised_learning.UnsupervisedLearnerPrimitiveB
 
         feats = self.audio_set._featurize(inputs.audio)
 
-        audio_vecs = pd.DataFrame(feats.tolist())
+        audio_vecs = pd.DataFrame(feats.tolist(), inputs.index)
         audio_vecs.columns = ['v{}'.format(i) for i in range(0, audio_vecs.shape[1])]
-        audio_vecs.index = inputs['d3mIndex']
-        audio_vecs.index.name = 'd3mIndex'
-        
-        df = container.DataFrame(audio_vecs, generate_metadata=True) 
-        
+
+        df = container.DataFrame(audio_vecs) # TODO: fix index setup
+        df.index.name = 'd3mIndex'
+
         logger.warning(df)
 
         return df
