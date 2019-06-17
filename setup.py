f40fc26a0cf4043b114aface69ec5ca0a29cd956
cbethune <cbethune@uncharted.software>
2019-06-16 22:55:16 -0400

    skip install of apex due to issue with bert-pretrained

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup(
         'sgm @ git+https://github.com/nowfred/sgm.git@v1.0.3#egg=sgm',
         'basenet @ git+https://github.com/bkj/basenet.git@903756540b89809ef458f35257287b937b333417',
         'rescal @ git+https://github.com/cdbethune/rescal.py.git@af2091c1d5521c987edd3be41627b9c563582fe8',
-        'apex @ git+https://github.com/NVIDIA/apex.git@47e3367fcd6636db6cd549bbb385a6e06a3861d0'
+        # Can cause errors with pretrained-bert: https://github.com/NVIDIA/apex/issues/156
+        #'apex @ git+https://github.com/NVIDIA/apex.git@47e3367fcd6636db6cd549bbb385a6e06a3861d0'
 
     ],
     entry_points={
