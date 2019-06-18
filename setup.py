cc4debc337ac28d740e6e648035cbc4a1f5877d1
cbethune <cbethune@uncharted.software>
2019-06-17 20:17:28 -0400

    update pytorch bert fork

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'sgm @ git+https://github.com/nowfred/sgm.git@v1.0.3#egg=sgm',
         'basenet @ git+https://github.com/bkj/basenet.git@903756540b89809ef458f35257287b937b333417',
         'rescal @ git+https://github.com/cdbethune/rescal.py.git@af2091c1d5521c987edd3be41627b9c563582fe8',
-        'pytorch-pretrained-bert @ git+https://github.com/cdbethune/pytorch-pretrained-BERT.git@806c1048d69985b44c928adfb682cc0940114fc5'
+        'pytorch-pretrained-bert @ git+https://github.com/cdbethune/pytorch-pretrained-BERT.git@6639b53a34760208700127e24fbbae93fb03429a'
         # Can cause errors with pretrained-bert: https://github.com/NVIDIA/apex/issues/156
         #'apex @ git+https://github.com/NVIDIA/apex.git@47e3367fcd6636db6cd549bbb385a6e06a3861d0'
 
