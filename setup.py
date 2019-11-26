acb6169d7c574f86c4c477002198534ccf27584e
Chris Bethune <cbethune@uncharted.software>
2019-11-25 22:19:39 -0500

    fixes pip syntax

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setup(
         'basenet @ git+https://github.com/bkj/basenet.git@903756540b89809ef458f35257287b937b333417',
         'rescal @ git+https://github.com/cdbethune/rescal.py.git@af2091c1d5521c987edd3be41627b9c563582fe8',
         'pytorch-pretrained-bert @ git+https://github.com/cdbethune/pytorch-pretrained-BERT.git@fb5a42d9de9385b146ba585d6d47ec36d27dcbca#egg=pytorch-pretrained-bert',
-        'ShapExplainers @ git+https://github.com/NewKnowledge/shap_explainers.git@7a2375795803211613e9583fd625ba5f6d5fbedd#egg=ShapExplainers'
+        'ShapExplainers @ git+https://github.com/NewKnowledge/shap_explainers.git@7a2375795803211613e9583fd625ba5f6d5fbedd#egg=ShapExplainers',
         # Can cause errors with pretrained-bert: https://github.com/NVIDIA/apex/issues/156
         #'apex @ git+https://github.com/NVIDIA/apex.git@47e3367fcd6636db6cd549bbb385a6e06a3861d0',
-        'torchvggish @ git+https://github.com/harritaylor/torchvggish/commit/f5ec66cb05029ddfdb9971f343d79408fac44c70'
+        'torchvggish @ git+https://github.com/harritaylor/torchvggish.git@f5ec66cb05029ddfdb9971f343d79408fac44c70#egg=torchvggish'
 
     ],
     entry_points={
