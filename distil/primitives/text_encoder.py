7f29bee0e34e5bab6c85726f49316770cb4761ef
Balazs Horanyi <balazs.horanyi@qntfy.com>
2019-12-16 11:21:37 -0700

    add tfidf encoder with categorical checks

--- distil/primitives/text_encoder.py
+++ distil/primitives/text_encoder.py
@@ -9,9 +9,8 @@ from d3m.primitive_interfaces import base
 import pandas as pd
 import numpy as np
 
-from distil.preprocessing.transformers import SVMTextEncoder
+from distil.preprocessing.transformers import SVMTextEncoder, TfidifEncoder
 from distil.primitives import utils as distil_utils
-
 
 __all__ = ('TextEncoderPrimitive',)
 
@@ -20,12 +19,21 @@ logger = logging.getLogger(__name__)
 Inputs = container.DataFrame
 Outputs = container.DataFrame
 
+
 class Hyperparams(hyperparams.Hyperparams):
     use_columns = hyperparams.Set(
         elements=hyperparams.Hyperparameter[int](-1),
         default=(),
         semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
         description="A set of column indices to force primitive to operate on. If any specified column cannot be parsed, it is skipped.",
+    )
+
+    encoder_type = hyperparams.Enumeration(
+        default='tfidf',
+        values=['svm', 'tfidf'],
+        semantic_types=[
+            'https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        description="Vectorization Strategy.",
     )
 
 class Params(params.Params):
@@ -83,15 +91,15 @@ class TextEncoderPrimitive(base.PrimitiveBase[Inputs, Outputs, Params, Hyperpara
         self._inputs = inputs
         # https://github.com/scikit-learn/scikit-learn/issues/14429#issuecomment-513887163
         if type(outputs) == container.pandas.DataFrame and outputs.shape[1] == 1:
-            outputs = outputs.values.reshape(outputs.shape[0],)
+            outputs = outputs.values.reshape(outputs.shape[0], )
         self._outputs = outputs
-
 
     def fit(self, *, timeout: float = None, iterations: int = None) -> base.CallResult[None]:
         logger.debug(f'Fitting {__name__}')
 
         # determine columns to operate on
-        cols = distil_utils.get_operating_columns(self._inputs, self.hyperparams['use_columns'], ('http://schema.org/Text',))
+        cols = distil_utils.get_operating_columns(self._inputs, self.hyperparams['use_columns'],
+                                                  ('http://schema.org/Text',))
 
         logger.debug(f'Found {len(cols)} columns to encode')
 
@@ -102,9 +110,15 @@ class TextEncoderPrimitive(base.PrimitiveBase[Inputs, Outputs, Params, Hyperpara
             return base.CallResult(None)
 
         for i, c in enumerate(self._cols):
-            self._encoders.append(SVMTextEncoder())
-            text_inputs = self._inputs.iloc[:,c]
-            self._encoders[i].fit_transform(text_inputs, self._outputs) # requires fit transform to fit SVM on vectorizer results
+            if self.hyperparams['encoder_type'] == 'svm':
+                self._encoders.append(SVMTextEncoder())
+            elif self.hyperparams['encoder_type'] == 'tfidf':
+                self._encoders.append(TfidifEncoder())
+            else:
+                raise Exception(f"{self.hyperparams['encoder_type']} is not a valid encoder type")
+            text_inputs = self._inputs.iloc[:, c]
+            self._encoders[i].fit_transform(text_inputs,
+                                            self._outputs)  # requires fit transform to fit SVM on vectorizer results
 
         return base.CallResult(None)
 
@@ -119,18 +133,20 @@ class TextEncoderPrimitive(base.PrimitiveBase[Inputs, Outputs, Params, Hyperpara
         encoded_cols_source = []
         # encode columns into a new dataframe
         for i, c in enumerate(self._cols):
-            text_inputs = outputs.iloc[:,c]
+            text_inputs = outputs.iloc[:, c]
             result = self._encoders[i].transform(text_inputs)
             for j in range(result.shape[1]):
                 encoded_idx = i * result.shape[1] + j
-                encoded_cols[(f'__text_{encoded_idx}')] = result[:,j]
+                encoded_cols[(f'__text_{encoded_idx}')] = result[:, j]
                 encoded_cols_source.append(c)
         # generate metadata for encoded columns
         encoded_cols.metadata = encoded_cols.metadata.generate(encoded_cols)
         for c in range(encoded_cols.shape[1]):
-            encoded_cols.metadata = encoded_cols.metadata.add_semantic_type((metadata_base.ALL_ELEMENTS, encoded_idx), 'http://schema.org/Float')
+            encoded_cols.metadata = encoded_cols.metadata.add_semantic_type(
+                (metadata_base.ALL_ELEMENTS, encoded_idx), 'http://schema.org/Float')
             col_dict = dict(encoded_cols.metadata.query((metadata_base.ALL_ELEMENTS, c)))
-            col_dict['source_column'] = outputs.metadata.query((metadata_base.ALL_ELEMENTS, encoded_cols_source[c]))['name']
+            col_dict['source_column'] = \
+                outputs.metadata.query((metadata_base.ALL_ELEMENTS, encoded_cols_source[c]))['name']
             encoded_cols.metadata = encoded_cols.metadata.update((metadata_base.ALL_ELEMENTS, c), col_dict)
 
         # append the encoded columns and remove the source columns
