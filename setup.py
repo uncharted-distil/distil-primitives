dc4b983e7749ecf7f6dce5559a112766c028ffd3
Chris Bethune <cbethune@uncharted.software>
2020-05-29 10:26:08 -0400

    forcing scikit-image version to remove pillow 7.1.1 incompatibility

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'basenet @ git+https://github.com/bkj/basenet.git@903756540b89809ef458f35257287b937b333417',
         'rescal @ git+https://github.com/cdbethune/rescal.py.git@af2091c1d5521c987edd3be41627b9c563582fe8',
         'pytorch-pretrained-bert @ git+https://github.com/cdbethune/pytorch-pretrained-BERT.git@fb5a42d9de9385b146ba585d6d47ec36d27dcbca#egg=pytorch-pretrained-bert',
+	    'scikit-image<=0.16.1', # need this to ensure compatiblity with pillow 7.1.1
         'ShapExplainers @ git+https://github.com/cdbethune/shap_explainers.git@1010e2fcd6206c23e6bb7e78905f2e345277afc0#egg=ShapExplainers',
         # Can cause errors with pretrained-bert: https://github.com/NVIDIA/apex/issues/156
         #'apex @ git+https://github.com/NVIDIA/apex.git@47e3367fcd6636db6cd549bbb385a6e06a3861d0',
