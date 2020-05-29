03dcac22ac93defbd2c950a3ba112240bd4d010a
Chris Bethune <cbethune@uncharted.software>
2020-05-29 08:48:47 -0400

    skips annotation generation for files that aren't primitive modules

--- generate_annotations.py
+++ generate_annotations.py
@@ -6,19 +6,26 @@ import json
 import importlib
 import inspect
 
-# List all the primitives
 PRIMITIVES_DIR = 'distil/primitives'
-primitives = os.listdir(PRIMITIVES_DIR)
+OUTPUT_DIR = 'annotations'
 
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+# List all the primitives
+primitives = os.listdir(PRIMITIVES_DIR)
 for primitive in primitives:
     f = primitive.replace('.py', '')
-    lib = importlib.import_module('distil.primitives.' + f)
+    try:
+        lib = importlib.import_module('distil.primitives.' + f)
+    except:
+        continue
     for l in dir(lib):
         if 'Primitive' in l and l != 'PrimitiveBase':
             pp = getattr(lib, l)
             print(f'Extracting {l}')
             md = pp.metadata.to_json_structure()
             name = md['python_path']
-            with open('annotations/' + name + '.json', 'w') as f:
+            with open(os.path.join(OUTPUT_DIR, name + '.json'), 'w') as f:
                 f.write(json.dumps(md, indent=4))
                 f.write('\n')
