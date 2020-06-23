72f74dfa9f148dc05388fca60861b6e91686b2de
Balazs Horanyi <balazs.horanyi@qntfy.com>
2020-06-23 13:19:25 -0600

    add new metrics to primitve metrics

--- distil/modeling/metrics.py
+++ distil/modeling/metrics.py
@@ -1,80 +1,86 @@
 import sys
 import numpy as np
 from sklearn import metrics as sklearn_metrics
-#from external import objectDetectionAP
+from d3m.metrics import HitsAtKMetric, MeanReciprocalRankMetric
 
+# from external import objectDetectionAP
+hits_at_k = HitsAtKMetric(5)  # todo how does this get set?
+mean_recriprocal_rank = MeanReciprocalRankMetric()
 metrics = {
-
     # classification
-    'f1Macro'                     : lambda act, pred: sklearn_metrics.f1_score(act, pred, average='macro'),
-    'f1Micro'                     : lambda act, pred: sklearn_metrics.f1_score(act, pred, average='micro'),
-    'f1'                          : lambda act, pred: sklearn_metrics.f1_score(act, pred),
-    'accuracy'                    : lambda act, pred: sklearn_metrics.accuracy_score(act, pred),
-
+    "f1Macro": lambda act, pred: sklearn_metrics.f1_score(act, pred, average="macro"),
+    "f1Micro": lambda act, pred: sklearn_metrics.f1_score(act, pred, average="micro"),
+    "f1": lambda act, pred: sklearn_metrics.f1_score(act, pred),
+    "accuracy": lambda act, pred: sklearn_metrics.accuracy_score(act, pred),
     # regression
-    'meanSquaredError'            : lambda act, pred: -1.0 * sklearn_metrics.mean_squared_error(act, pred),
-    'meanAbsoluteError'           : lambda act, pred: -1.0 * sklearn_metrics.mean_absolute_error(act, pred),
-    'rootMeanSquaredError'        : lambda act, pred: -1.0 * np.sqrt(sklearn_metrics.mean_squared_error(act, pred)),
-    'rootMeanSquaredErrorAvg'     : lambda act, pred: -1.0 * np.sqrt(sklearn_metrics.mean_squared_error(act, pred)),
-    'rSquared'                    : lambda act, pred: -1.0 * sklearn_metrics.r2_score(act, pred),
-
+    "meanSquaredError": lambda act, pred: -1.0
+    * sklearn_metrics.mean_squared_error(act, pred),
+    "meanAbsoluteError": lambda act, pred: -1.0
+    * sklearn_metrics.mean_absolute_error(act, pred),
+    "rootMeanSquaredError": lambda act, pred: -1.0
+    * np.sqrt(sklearn_metrics.mean_squared_error(act, pred)),
+    "rootMeanSquaredErrorAvg": lambda act, pred: -1.0
+    * np.sqrt(sklearn_metrics.mean_squared_error(act, pred)),
+    "rSquared": lambda act, pred: -1.0 * sklearn_metrics.r2_score(act, pred),
     # clustering
-    'normalizedMutualInformation' : sklearn_metrics.normalized_mutual_info_score,
-
+    "normalizedMutualInformation": sklearn_metrics.normalized_mutual_info_score,
+    "meanReciprocalRank": lambda act, pred: mean_recriprocal_rank.score(act, pred),
+    "hitsAtK": lambda act, pred: hits_at_k.score(act, pred)
     # object detection
     #'objectDetectionAP' : lambda act, pred: objectDetectionAP(act, pred)[-1],
 }
 
 classification_metrics = [
-    'f1Macro',
-    'f1Micro',
-    'f1',
-    'accuracy'
+    "f1Macro",
+    "f1Micro",
+    "f1",
+    "accuracy",
+    "meanReciprocalRank",
+    "hitsAtK",
 ]
 
 regression_metrics = [
-    'meanSquaredError',
-    'meanAbsoluteError',
-    'rootMeanSquaredError',
-    'rootMeanSquaredErrorAvg',
-    'rSquared',
+    "meanSquaredError",
+    "meanAbsoluteError",
+    "rootMeanSquaredError",
+    "rootMeanSquaredErrorAvg",
+    "rSquared",
 ]
 
 clustering_metrics = [
-    'normalizedMutualInformation',
+    "normalizedMutualInformation",
 ]
 
 
 def translate_d3m_metric(metric):
     lookup = {
-        'f1Macro'                     : 'f1_macro',
-        'f1Micro'                     : 'f1_micro',
-        'f1'                          : 'f1',
-        'accuracy'                    : 'accuracy',
-
-        'rSquared'                : 'r_squared',
-        'meanSquaredError'        : 'mean_squared_error',
-        'rootMeanSquaredError'    : 'root_mean_squared_error',
-        'rootMeanSquaredErrorAvg' : 'root_mean_squared_error_avg',
-        'meanAbsoluteError'       : 'mean_absolute_error',
-
-        'normalizedMutualInformation' : 'normalized_mutual_information',
+        "f1Macro": "f1_macro",
+        "f1Micro": "f1_micro",
+        "f1": "f1",
+        "accuracy": "accuracy",
+        "rSquared": "r_squared",
+        "meanSquaredError": "mean_squared_error",
+        "rootMeanSquaredError": "root_mean_squared_error",
+        "rootMeanSquaredErrorAvg": "root_mean_squared_error_avg",
+        "meanAbsoluteError": "mean_absolute_error",
+        "normalizedMutualInformation": "normalized_mutual_information",
     }
-    assert metric in lookup, '%s not in lookup' % metric
+    assert metric in lookup, "%s not in lookup" % metric
     return lookup[metric]
+
 
 def translate_proto_metric(proto_metric):
     lookup = {
-        'F1_MACRO': 'f1Macro',
-        'F1_MICRO': 'f1Micro',
-        'F1': 'f1',
-        'ACCURACY': 'accuracy',
-        'MEAN_SQUARED_ERROR': 'meanSquaredError',
-        'ROOT_MEAN_SQUARED_ERROR': 'rootMeanSquaredError',
-        'ROOT_MEAN_SQUARED_ERROR_AVG': 'rootMeanSquaredErrorAvg',
-        'R_SQUARED': 'rSquared', # mapped for now,
-        'MEAN_ABSOLUTE_ERROR': 'meanAbsoluteError',
-        'NORMALIZED_MUTUAL_INFORMATION': 'normalizedMutualInformation',
+        "F1_MACRO": "f1Macro",
+        "F1_MICRO": "f1Micro",
+        "F1": "f1",
+        "ACCURACY": "accuracy",
+        "MEAN_SQUARED_ERROR": "meanSquaredError",
+        "ROOT_MEAN_SQUARED_ERROR": "rootMeanSquaredError",
+        "ROOT_MEAN_SQUARED_ERROR_AVG": "rootMeanSquaredErrorAvg",
+        "R_SQUARED": "rSquared",  # mapped for now,
+        "MEAN_ABSOLUTE_ERROR": "meanAbsoluteError",
+        "NORMALIZED_MUTUAL_INFORMATION": "normalizedMutualInformation",
     }
-    assert proto_metric in lookup, '%s not in lookup' % proto_metric
+    assert proto_metric in lookup, "%s not in lookup" % proto_metric
     return lookup[proto_metric]
