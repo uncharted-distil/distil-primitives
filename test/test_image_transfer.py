ffbcb341884fd8b9ba4fa554b078fc4f3bdf5bf1
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2020-06-23 15:46:43 -0400

    updated image transfer to take in filename hyperparam

---
+++ test/test_image_transfer.py
@@ -0,0 +1,54 @@
+import unittest
+from os import path
+import sys
+
+from d3m import container
+from d3m.metadata import base as metadata_base
+from common_primitives import dataset_to_dataframe, dataframe_image_reader
+from distil.primitives.image_transfer import ImageTransferPrimitive
+import utils as test_utils
+
+class ImageTransferPrimitveTestCase(unittest.TestCase):
+
+    _dataset_path = path.abspath(path.join(path.dirname(__file__), 'image_dataset_1'))
+
+    def test_basic(self):
+        dataset = test_utils.load_dataset(self._dataset_path)
+
+        dataframe_hyperparams_class = dataset_to_dataframe.DatasetToDataFramePrimitive.metadata.get_hyperparams()
+        dataframe_primitive = dataset_to_dataframe.DatasetToDataFramePrimitive(hyperparams=dataframe_hyperparams_class.defaults().replace({'dataframe_resource': '0'}))
+        dataframe = dataframe_primitive.produce(inputs=dataset).value
+        image_hyperparams_class = dataframe_image_reader.DataFrameImageReaderPrimitive.metadata.get_hyperparams()
+        image_primitive = dataframe_image_reader.DataFrameImageReaderPrimitive(hyperparams=image_hyperparams_class.defaults().replace({'return_result': 'replace'}))
+        images = image_primitive.produce(inputs=dataframe).value
+
+        image_transfer_hyperparams = ImageTransferPrimitive.metadata.get_hyperparams()
+        primitive_volumes = ImageTransferPrimitive.metadata.get_volumes()
+        volumes = {primitive_volumes[0]['key']: '/Users/vkorapaty/data/static_resources/' + primitive_volumes[0]['file_digest']}
+        image_transfer_primitive = ImageTransferPrimitive(hyperparams=image_transfer_hyperparams.defaults().replace({'filename_col': 0}), volumes=volumes)
+        result = image_transfer_primitive.produce(inputs=images).value
+        self.assertEqual(result.shape[0], 5)
+        self.assertEqual(result.shape[1], 512)
+
+    def test_no_hyperparam(self):
+        dataset = test_utils.load_dataset(self._dataset_path)
+
+        dataframe_hyperparams_class = dataset_to_dataframe.DatasetToDataFramePrimitive.metadata.get_hyperparams()
+        dataframe_primitive = dataset_to_dataframe.DatasetToDataFramePrimitive(hyperparams=dataframe_hyperparams_class.defaults().replace({'dataframe_resource': '0'}))
+        dataframe = dataframe_primitive.produce(inputs=dataset).value
+        image_hyperparams_class = dataframe_image_reader.DataFrameImageReaderPrimitive.metadata.get_hyperparams()
+        image_primitive = dataframe_image_reader.DataFrameImageReaderPrimitive(hyperparams=image_hyperparams_class.defaults().replace({'return_result': 'replace'}))
+        images = image_primitive.produce(inputs=dataframe).value
+        images.metadata = images.metadata.add_semantic_type((metadata_base.ALL_ELEMENTS, images.metadata.get_column_index_from_column_name('filename')), 'https://schema.org/ImageObject')
+        
+        image_transfer_hyperparams = ImageTransferPrimitive.metadata.get_hyperparams()
+        primitive_volumes = ImageTransferPrimitive.metadata.get_volumes()
+        volumes = {primitive_volumes[0]['key']: '/Users/vkorapaty/data/static_resources/' + primitive_volumes[0]['file_digest']}
+        image_transfer_primitive = ImageTransferPrimitive(hyperparams=image_transfer_hyperparams.defaults(), volumes=volumes)
+        result = image_transfer_primitive.produce(inputs=images).value
+        self.assertEqual(result.shape[0], 5)
+        self.assertEqual(result.shape[1], 512)
+
+
+if __name__ == '__main__':
+    unittest.main()
