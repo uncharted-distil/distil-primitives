a7d428c4e0dd0459cd18159f97d3db6379a306d3
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2020-07-10 19:31:46 -0400

    MI ranking is normalized now

--- distil/primitives/mi_ranking.py
+++ distil/primitives/mi_ranking.py
@@ -1,13 +1,21 @@
 import os
 import typing
+from math import log
 
 import numpy as np
+from scipy.sparse import issparse
+from scipy.special import digamma, gamma
 import pandas as pd  # type: ignore
 from d3m import container, utils as d3m_utils
+from d3m import exceptions
 from d3m.metadata import base as metadata_base, hyperparams
 from d3m.primitive_interfaces import base, transformer
 from distil.utils import CYTHON_DEP
 from sklearn.feature_selection import mutual_info_regression, mutual_info_classif
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import utils as skl_utils
+from sklearn.neighbors import NearestNeighbors
 import version
 
 __all__ = ('MIRankingPrimitive',)
@@ -18,6 +26,11 @@ class Hyperparams(hyperparams.Hyperparams):
         default=None,
         semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
         description='Index of target feature to rank against.'
+    )
+    k = hyperparams.Hyperparameter[typing.Optional[int]](
+        default=3,
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        description='Number of clusters for k-nearest neighbors'
     )
 
 class MIRankingPrimitive(transformer.TransformerPrimitiveBase[container.DataFrame,
@@ -176,12 +189,16 @@ class MIRankingPrimitive(transformer.TransformerPrimitiveBase[container.DataFram
             ranked_features_np = mutual_info_classif(feature_np,
                                                      target_np,
                                                      discrete_features=discrete_flags,
+                                                     n_neighbors=self.hyperparams['k'],
                                                      random_state=self._random_seed)
         else:
             ranked_features_np = mutual_info_regression(feature_np,
                                                         target_np,
                                                         discrete_features=discrete_flags,
+                                                        n_neighbors=self.hyperparams['k'],
                                                         random_state=self._random_seed)
+
+        ranked_features_np = self._normalize(ranked_features_np, feature_np, target_np, discrete, discrete_flags)
 
         # merge back into a single list of col idx / rank value tuples
         data: typing.List[typing.Tuple[int, str, float]] = []
@@ -192,3 +209,226 @@ class MIRankingPrimitive(transformer.TransformerPrimitiveBase[container.DataFram
         results = results.sort_values(by=['rank'], ascending=False).reset_index(drop=True)
 
         return base.CallResult(results)
+
+    def _normalize(self, ranked_features, feature_np, target_np, discrete, discrete_flags):
+        normalized_ranked_features = np.empty(ranked_features.shape[0])
+        # ksg_entropies = np.empty(ranked_features.shape[0])
+        # naive_entropies = np.empty(ranked_features.shape[0])
+        if discrete:
+            target_entropy = self._discrete_entropy(target_np)
+            for i in range(ranked_features.shape[0]):
+                if discrete_flags[i]:
+                    normalized_ranked_features[i] = ranked_features[i] / np.sqrt(self._discrete_entropy(feature_np[:, i]) * target_entropy)
+                    if normalized_ranked_features[i] > 1:
+                        normalized_ranked_features[i] = 1.0
+                else:
+                    ksg_entropy, naive_entropy = self._continuous_entropy(feature_np[:, i])
+                    ksg_mi = ranked_features[i] / np.sqrt(ksg_entropy * target_entropy)
+                    naive_mi = ranked_features[i] / np.sqrt(naive_entropy * target_entropy)
+                    if abs(ksg_mi - 1) < abs(naive_mi - 1):
+                        if ksg_mi > 1:
+                            normalized_ranked_features[i] = 1.0
+                        else:
+                            normalized_ranked_features[i] = ksg_mi
+                    else:
+                        if naive_mi > 1:
+                            normalized_ranked_features[i] = 1.0
+                        else:
+                            normalized_ranked_features[i] = naive_mi
+
+            # logic if we want to be consistent with which entropy we use throughout
+            # if discrete_flags.sum() < discrete_flags.shape[0]:
+            #     max_ksg_entropy = np.max(ksg_entropies)
+            #     max_naive_entropy = np.max(naive_entropies)
+            #     if max_ksg_entropy > 1 and max_naive_entropy > 1:
+            #         if ksg_entropies.sum() < naive_entropies.sum():
+            #             ksg_entropies[ksg_entropies > 1] = 1.0
+            #             normalized_ranked_features[~discrete_flags] = ksg_entropies[~discrete_flags]
+            #         else:
+            #             naive_entropies[naive_entropies > 1] = 1.0
+            #             normalized_ranked_features[~discrete_flags] = naive_entropies[~discrete_flags]
+            #     elif max_ksg_entropy > 1 or max_naive_entropy > max_ksg_entropy:
+            #         normalized_ranked_features[~discrete_flags] = naive_entropies[~discrete_flags]
+            #     elif max_naive_entropy > 1 or max_naive_entropy < max_ksg_entropy:
+            #         normalized_ranked_features[~discrete_flags] = ksg_entropies[~discrete_flags]
+        else:
+            target_ksg_entropy, target_naive_entropy = self._continuous_entropy(target_np)
+            # if target_ksg_entropy > target_naive_entropy:
+            #     discrete_entropy = target_naive_entropy
+            # else:
+            #     discrete_entropy = target_ksg_entropy
+            for i in range(ranked_features.shape[0]):
+                if discrete_flags[i]:
+                    feature_entropy = self._discrete_entropy(feature_np[:, i])
+                    ksg_mi = ranked_features[i] / np.sqrt(feature_entropy * target_ksg_entropy)
+                    naive_mi = ranked_features[i] / np.sqrt(feature_entropy * target_naive_entropy)
+                else:
+                    ksg_entropy, naive_entropy = self._continuous_entropy(feature_np[:, i])
+                    ksg_mi = ranked_features[i] / np.sqrt(ksg_entropy * target_ksg_entropy)
+                    naive_mi = ranked_features[i] / np.sqrt(naive_entropy * target_naive_entropy)
+                if abs(ksg_mi - 1) < abs(naive_mi - 1):
+                    if ksg_mi > 1:
+                        normalized_ranked_features[i] = 1.0
+                    else:
+                        normalized_ranked_features[i] = ksg_mi
+                else:
+                    if naive_mi > 1:
+                        normalized_ranked_features[i] = 1.0
+                    else:
+                        normalized_ranked_features[i] = naive_mi
+                    
+
+        return normalized_ranked_features
+
+    def _discrete_entropy(self, labels):
+        """Calculates the entropy for a labeling.
+        Parameters
+        ----------
+        labels : int array, shape = [n_samples]
+            The labels
+        Notes
+        -----
+        The logarithm used is the natural logarithm (base-e).
+        """
+        if len(labels) == 0:
+            return 1.0
+        label_idx = np.unique(labels, return_inverse=True)[1]
+        pi = np.bincount(label_idx).astype(np.float64)
+        pi = pi[pi > 0]
+        pi_sum = np.sum(pi)
+        # log(a / b) should be calculated as log(a) - log(b) for
+        # possible loss of precision
+        entropy = -np.sum((pi / pi_sum) * (np.log(pi) - log(pi_sum)))
+        if entropy <= 0:
+                raise exceptions.InvalidArgumentValueError('inputs has too many non-unique values or not enough values possibly')
+        return entropy
+
+    def _continuous_entropy(self, x):
+        k = self.hyperparams['k']
+        sorted_x = np.sort(x)
+
+        eps_distances = np.empty(x.shape[0])
+        k_all = np.full(x.shape, k - 1)
+        for i in range(x.shape[0]):
+            eps = 0
+            # need to prevent having an epsilon value of 0
+            # as a fall back, increase k to find farther neighbours
+            while eps == 0 and k_all[i] < x.shape[0]:
+                k_all[i] += 1
+                eps = self._eps(sorted_x, i, k_all[i])
+            if eps == 0 and k_all[i] == x.shape[0]:
+                raise exceptions.InvalidArgumentValueError('inputs has too many non-unique values or not enough values possibly')
+            eps_distances[i] = eps
+
+        log_mean = np.mean(np.log(eps_distances))
+        if log_mean == float('-inf'):
+            raise exceptions.InvalidArgumentValueError('inputs has too many non-unique values or not enough values possibly')
+
+        # if method == 0:
+        # this estimation is https://arxiv.org/pdf/cond-mat/0305641.pdf
+        # the KSG estimator
+        ksg_entropy = - np.mean(digamma(k_all)) + digamma(x.shape[0]) + log_mean
+        # elif method == 1:
+        # a more naive estimator
+        # this estimation is from http://papers.neurips.cc/paper/3417-estimation-of-information-theoretic-measures-for-continuous-random-variables.pdf
+        naive_entropy = - np.mean(np.log(k_all/(x.shape[0] - 1) * gamma(1.5) / pow(3.14159, 0.5) * 1 / eps_distances)) - np.mean(digamma(k_all))
+        return ksg_entropy, naive_entropy
+
+    def _eps(self, x, i, k):
+        return 2 * abs(x[self._k_closest_neighbour(x, i, k)] - x[i])
+
+    # assumes a is sorted
+    def _k_closest_neighbour(self, a, i, k):
+        length = len(a)
+        j = 0
+        if i == 0:
+            l = -1
+            r = i + 1
+        elif i == length - 1:
+            r = length
+            l = i - 1
+        else:
+            r = i + 1
+            l = i - 1
+        kth_closest = i
+        while j < k:
+            if r == length:
+                if l == -1:
+                    if abs(a[0] - a[i]) > abs(a[length - 1] - a[i]):
+                        return 0
+                    return length - 1
+                elif abs(a[r - 1] - a[i]) <= abs(a[l] - a[i]):
+                    kth_closest = l
+                    l -= 1
+            elif l == -1 and abs(a[r] - a[i]) >= abs(a[0] - a[i]):
+                kth_closest = r
+                r += 1
+            elif r < length and abs(a[r] - a[i]) <= abs(a[l] - a[i]):
+                kth_closest = r
+                r += 1
+            elif l > -1 and abs(a[r] - a[i]) >= abs(a[l] - a[i]):
+                kth_closest = l
+                l -= 1
+            j += 1
+        return kth_closest
+
+    # def _mi_reg(self, X, y, discrete_features):
+    #     sample_size = X.shape[0]
+    #     feature_size = X.shape[1]
+    #     continuous_mask = ~discrete_features
+
+    #     if np.any(continuous_mask):
+    #         if issparse(X):
+    #             raise ValueError("Sparse matrix `X` can't have continuous features.")
+
+    #         rng = skl_utils.check_random_state(self._random_seed)
+    #         X[:, continuous_mask] = preprocessing.scale(X[:, continuous_mask], with_mean=False, copy=False)
+
+    #         X = X.astype(float, **skl_utils.fixes._astype_copy_false(X))
+    #         means = np.maximum(1, np.mean(np.abs(X[:, continuous_mask]), axis=0))
+    #         X[:, continuous_mask] += 1e-10 * means * rng.randn(sample_size, np.sum(continuous_mask))
+
+    #     y = preprocessing.scale(y, with_mean=False)
+    #     y += 1e-10 * np.maximum(1, np.mean(np.abs(y))) * rng.randn(sample_size)
+
+    #     normalised_mi = np.empty([1, feature_size])
+
+    #     for i in range(feature_size):
+    #         feature = X[:, i]
+    #         is_continuous_feature = continuous_mask[i]
+    #         if not is_continuous_feature:
+    #             feature = feature.reshape((-1, 1))
+
+    #             radii = np.empty(sample_size)
+    #             label_counts = np.empty(sample_size)
+    #             k_all = np.empty(sample_size)
+    #             nn = NearestNeighbors()
+    #             for label in np.unique(y):
+    #                 mask = d == label
+    #                 count = np.sum(mask)
+    #                 if count > 1:
+    #                     k = min(self.hyperparams['k'], count - 1)
+    #                     nn.set_params(n_neighbors=k)
+    #                     nn.fit(c[mask])
+    #                     r = nn.kneighbors()[0]
+    #                     radius[mask] = np.nextafter(r[:, -1], 0)
+    #                     k_all[mask] = k
+    #                 label_counts[mask] = count
+                
+    #             mask = label_counts > 1
+    #             n_samples = np.sum(mask)
+    #             label_counts = label_counts[mask]
+    #             k_all = k_all[mask]
+    #             c = c[mask]
+    #             radius = radius[mask]
+
+    #             nn.set_params(algorithm='kd_tree')
+    #             nn.fit(feature)
+    #             ind = nn.radius_neighbors(radius=radius, return_distance=False)
+    #             m_all = np.array([i.size for i in ind])
+
+    #             I_XY = max(0, (digamma(feature_size) + np.mean(digamma(k_all)) -
+    #                     np.mean(digamma(label_counts)) - np.mean(digamma(m_all + 1))))
+    #             H_X = 0.5
+    #             H_Y = 0.5
+    #             normalised_mi[i] = I_XY / (H_X + H_Y)
--- test/test_mi_ranking.py
+++ test/test_mi_ranking.py
@@ -22,7 +22,7 @@ import pandas as pd
 import numpy as np
 
 from d3m import container
-from d3m.primitives.feature_selection.mi_ranking import DistilMIRanking as MIRanking
+from distil.primitives.mi_ranking import MIRankingPrimitive as MIRanking
 from d3m.metadata import base as metadata_base
 
 
@@ -46,7 +46,7 @@ class MIRankingPrimitiveTestCase(unittest.TestCase):
         # verify the output
         self.assertListEqual(list(result_dataframe['idx']), [2, 5, 3])
         self.assertListEqual(list(result_dataframe['name']), ['bravo', 'echo', 'charlie'])
-        expected_ranks = [1.342857, 0.861607, 0.0]
+        expected_ranks = [1.0, 0.756548, 0.0]
         for i, r in enumerate(result_dataframe['rank']):
             self.assertAlmostEqual(r, expected_ranks[i], places=6)
 
@@ -66,7 +66,32 @@ class MIRankingPrimitiveTestCase(unittest.TestCase):
         # verify the output
         self.assertListEqual(list(result_dataframe['idx']), [1, 5, 3])
         self.assertListEqual(list(result_dataframe['name']), ['alpha', 'echo', 'charlie'])
-        expected_ranks = [1.405357, 0.422024, 0.0]
+        expected_ranks = [0.965127, 0.461964, 0.0]
+        for i, r in enumerate(result_dataframe['rank']):
+            self.assertAlmostEqual(r, expected_ranks[i], places=6)
+
+    def test_full_mutual_info(self) -> None:
+        dataframe = self._load_data(alpha_class=float, charlie_class=float)
+
+        hyperparams_class = \
+            MIRanking.metadata.query()['primitive_code']['class_type_arguments']['Hyperparams']
+        hyperparams = hyperparams_class.defaults().replace(
+            {
+                'target_col_index': 2
+            }
+        )
+        mi_ranking = MIRanking(hyperparams=hyperparams)
+        # dataframe.drop(columns=['charlie', 'delta', 'echo'], inplace=True)
+        # dataframe['bravo'][dataframe['bravo'] == 4] = 3
+        dataframe['bravo'] = [1, 0.5, 10, 6.7, 6.9, 2.3, 5.5, 7.3, 9]
+        dataframe['alpha'] = [1, 0.5, 10, 6.7, 6.9, 2.3, 5.5, 7.3, 9]
+        dataframe['charlie'] = [2, 2.5, 9, 5.7, 7.9, 1.3, 6.5, 8.3, 8]
+        result_dataframe = mi_ranking.produce(inputs=dataframe).value
+
+        # verify the output
+        self.assertListEqual(list(result_dataframe['idx']), [1, 3, 5])
+        self.assertListEqual(list(result_dataframe['name']), ['alpha', 'charlie', 'echo'])
+        expected_ranks = [0.344232, 0.227931, 0.0]
         for i, r in enumerate(result_dataframe['rank']):
             self.assertAlmostEqual(r, expected_ranks[i], places=6)
 
@@ -106,7 +131,7 @@ class MIRankingPrimitiveTestCase(unittest.TestCase):
         self.assertListEqual(list(result_dataframe['name']), [])
         self.assertListEqual(list(result_dataframe['rank']), [])
 
-    def _load_data(cls, bad_features: typing.Sequence[int]=[]) -> container.DataFrame:
+    def _load_data(cls, bad_features: typing.Sequence[int]=[], alpha_class=int, charlie_class=int) -> container.DataFrame:
         dataset_doc_path = path.join(cls._dataset_path, 'datasetDoc.json')
 
         # load the dataset and convert resource 0 to a dataframe
@@ -118,24 +143,33 @@ class MIRankingPrimitiveTestCase(unittest.TestCase):
         dataframe.metadata = dataframe.metadata.update((metadata_base.ALL_ELEMENTS, 0),
                                                         {'structural_type': int})
         dataframe.metadata = dataframe.metadata.update((metadata_base.ALL_ELEMENTS, 1),
-                                                        {'structural_type': int})
+                                                        {'structural_type': alpha_class})
         dataframe.metadata = dataframe.metadata.update((metadata_base.ALL_ELEMENTS, 2),
                                                         {'structural_type': float})
         dataframe.metadata = dataframe.metadata.update((metadata_base.ALL_ELEMENTS, 3),
-                                                        {'structural_type': int})
+                                                        {'structural_type': charlie_class})
         dataframe.metadata = dataframe.metadata.update((metadata_base.ALL_ELEMENTS, 4),
                                                         {'structural_type': str})
         dataframe.metadata = dataframe.metadata.update((metadata_base.ALL_ELEMENTS, 5),
                                                         {'structural_type': int})
 
         # set the semantic type
-        dataframe.metadata = dataframe.metadata.\
-            add_semantic_type((metadata_base.ALL_ELEMENTS, 1),
-                            'https://metadata.datadrivendiscovery.org/types/CategoricalData')
+        if alpha_class == int:
+            dataframe.metadata = dataframe.metadata.\
+                add_semantic_type((metadata_base.ALL_ELEMENTS, 1),
+                                'https://metadata.datadrivendiscovery.org/types/CategoricalData')
+        elif alpha_class == float:
+            dataframe.metadata = dataframe.metadata.\
+                add_semantic_type((metadata_base.ALL_ELEMENTS, 1), 'http://schema.org/Float')
+
         dataframe.metadata = dataframe.metadata.\
             add_semantic_type((metadata_base.ALL_ELEMENTS, 2), 'http://schema.org/Float')
-        dataframe.metadata = dataframe.metadata.\
-            add_semantic_type((metadata_base.ALL_ELEMENTS, 3), 'http://schema.org/Boolean')
+        if charlie_class == int:
+            dataframe.metadata = dataframe.metadata.\
+                add_semantic_type((metadata_base.ALL_ELEMENTS, 3), 'http://schema.org/Boolean')
+        elif charlie_class == float:
+            dataframe.metadata = dataframe.metadata.\
+                add_semantic_type((metadata_base.ALL_ELEMENTS, 3), 'http://schema.org/Float')
         dataframe.metadata = dataframe.metadata.\
             add_semantic_type((metadata_base.ALL_ELEMENTS, 4), 'http://schema.org/Text')
         dataframe.metadata = dataframe.metadata.\
@@ -159,12 +193,12 @@ class MIRankingPrimitiveTestCase(unittest.TestCase):
         dataframe = dataframe.replace(r'^\s*$', np.nan, regex=True)
 
         # cast the dataframe to raw python types
-        dataframe['d3mIndex'].astype(int)
-        dataframe['alpha'].astype(int)
-        dataframe['bravo'].astype(float)
-        dataframe['charlie'].astype(int)
-        dataframe['delta'].astype(str)
-        dataframe['echo'].astype(float)
+        dataframe['d3mIndex'] = dataframe['d3mIndex'].astype(int)
+        dataframe['alpha'] = dataframe['alpha'].astype(alpha_class)
+        dataframe['bravo'] = dataframe['bravo'].astype(float)
+        dataframe['charlie'] = dataframe['charlie'].astype(alpha_class)
+        dataframe['delta'] = dataframe['delta'].astype(str)
+        dataframe['echo'] = dataframe['echo'].astype(float)
 
         return dataframe
 
