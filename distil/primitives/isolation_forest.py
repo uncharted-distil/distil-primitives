2edfce8747642a943aad9c0fed6269e6c226c488
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2021-01-07 13:55:05 -0500

    added isolation forest

---
+++ distil/primitives/isolation_forest.py
@@ -0,0 +1,133 @@
+import logging
+import os
+import math
+from typing import List, Optional
+import typing
+
+import numpy as np
+import pandas as pd
+
+from d3m import container, utils
+from d3m.metadata import base as metadata_base, hyperparams, params
+from d3m.primitive_interfaces import base, transformer
+from d3m.primitive_interfaces.supervised_learning import PrimitiveBase
+
+from sklearn.ensemble import IsolationForest
+
+from distil.utils import CYTHON_DEP
+import version
+
+__all__ = ("PrefeaturisedPoolingPrimitive",)
+
+logger = logging.getLogger(__name__)
+
+
+class Hyperparams(hyperparams.Hyperparams):
+    n_jobs = hyperparams.Hyperparameter[int](
+        default=64,
+        semantic_types=[
+            "https://metadata.datadrivendiscovery.org/types/ControlParameter"
+        ],
+        description="The value of the n_jobs parameter for the joblib library",
+    )
+
+
+class Params(params.Params):
+    model: IsolationForest
+    needs_fit: bool
+
+
+class IsolationForestPrimitive(
+    PrimitiveBase[container.DataFrame, container.DataFrame, Params, Hyperparams]
+):
+    """
+    Uses scikit learn's Isolated Forest primitive to detect and label anomalies.
+    """
+
+    metadata = metadata_base.PrimitiveMetadata(
+        {
+            "id": "793f0b17-7413-4962-9f1d-0b285540b21f",
+            "version": version.__version__,
+            "name": "Isolation Forest",
+            "python_path": "d3m.primitives.classification.isolation_forest.IsolationForestPrimitive",
+            "source": {
+                "name": "Distil",
+                "contact": "mailto:vkorapaty@uncharted.software",
+                "uris": [
+                    "https://github.com/uncharted-distil/distil-primitives/primitives/isolation_forest.py",
+                    "https://github.com/uncharted-distil/distil-primitives",
+                ],
+            },
+            "installation": [
+                CYTHON_DEP,
+                {
+                    "type": metadata_base.PrimitiveInstallationType.PIP,
+                    "package_uri": "git+https://github.com/uncharted-distil/distil-primitives.git@{git_commit}#egg=distil-primitives".format(
+                        git_commit=utils.current_git_commit(os.path.dirname(__file__)),
+                    ),
+                },
+            ],
+            "algorithm_types": [
+                metadata_base.PrimitiveAlgorithmType.BINARY_CLASSIFICATION,
+            ],
+            "primitive_family": metadata_base.PrimitiveFamily.CLASSIFICATION,
+        },
+    )
+
+    def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0) -> None:
+
+        super().__init__(hyperparams=hyperparams, random_seed=random_seed)
+        self._model = IsolationForest(random_state=np.random.RandomState(random_seed))
+
+    def set_training_data(
+        self, *, inputs: container.DataFrame, outputs: container.DataFrame
+    ) -> None:
+        self._inputs = inputs
+        self._outputs = outputs
+        self._needs_fit = True
+
+    def fit(
+        self, *, timeout: float = None, iterations: int = None
+    ) -> base.CallResult[None]:
+        logger.debug(f"Fitting {__name__}")
+
+        if self._needs_fit:
+            self._model.fit(self._inputs)
+            self._needs_fit = False
+
+        return base.CallResult(None)
+
+    def produce(
+        self,
+        *,
+        inputs: container.DataFrame,
+        timeout: float = None,
+        iterations: int = None,
+    ) -> base.CallResult[container.DataFrame]:
+
+        if self._needs_fit:
+            self.fit()
+
+        index = inputs.index
+        result = self._model.predict(inputs)
+
+        result_df = container.DataFrame(
+            {
+                "label": result,
+            },
+            generate_metadata=True,
+        )
+        result_df.set_index(index, inplace=True)
+
+        return base.CallResult(result_df)
+
+    def get_params(self) -> Params:
+        return Params(
+            model=self._model,
+            needs_fit=self._needs_fit,
+        )
+
+    def set_params(self, *, params: Params) -> None:
+        self._model = params["model"]
+        self._needs_fit = params["needs_fit"]
+        return
---
+++ test/test_isolation_forest.py
@@ -0,0 +1,125 @@
+import unittest
+from os import path
+import csv
+import typing
+import pandas as pd
+import numpy as np
+
+from d3m import container
+from common_primitives.dataset_to_dataframe import DatasetToDataFramePrimitive
+from common_primitives.column_parser import ColumnParserPrimitive
+from distil.primitives.isolation_forest import IsolationForestPrimitive
+from d3m.metadata import base as metadata_base
+import utils as test_utils
+
+from sklearn.preprocessing import LabelEncoder
+
+
+class IsolationForestPrimitiveTestCase(unittest.TestCase):
+
+    _dataset_path = path.abspath(path.join(path.dirname(__file__), "tabular_dataset_2"))
+
+    def test_basic(self) -> None:
+        dataset = test_utils.load_dataset(self._dataset_path)
+        dataframe = test_utils.get_dataframe(dataset, "learningData")
+        dataframe.drop(columns=["delta", "echo"], inplace=True)
+
+        hyperparams_class = IsolationForestPrimitive.metadata.query()["primitive_code"][
+            "class_type_arguments"
+        ]["Hyperparams"]
+        hyperparams = hyperparams_class.defaults().replace({"n_jobs": -1})
+
+        isp = IsolationForestPrimitive(hyperparams=hyperparams)
+        isp.set_training_data(
+            inputs=dataframe[["alpha", "bravo"]],
+            outputs=pd.DataFrame({"charlie": dataframe["charlie"].astype(int)}),
+        )
+        isp.fit()
+        results = isp.produce(inputs=dataframe[["alpha", "bravo"]]).value
+
+        self.assertListEqual(
+            list(results["label"]), [-1, -1, -1, -1, -1, -1, -1, -1, -1]
+        )
+
+    # def test_acled(self) -> None:
+    #     dataset = test_utils.load_dataset(
+    #         "/Users/vkorapaty/data/datasets/seed_datasets_current/LL0_acled_reduced_MIN_METADATA/LL0_acled_reduced_MIN_METADATA"
+    #     )
+
+    #     hyperparams_class = DatasetToDataFramePrimitive.metadata.query()[
+    #         "primitive_code"
+    #     ]["class_type_arguments"]["Hyperparams"]
+    #     dataframe_primitive = DatasetToDataFramePrimitive(
+    #         hyperparams=hyperparams_class.defaults()
+    #     )
+    #     dataframe = dataframe_primitive.produce(inputs=dataset).value
+
+    #     dataframe.metadata = dataframe.metadata.update(
+    #         (metadata_base.ALL_ELEMENTS, 6), {"structural_type": int}
+    #     )
+    #     le = LabelEncoder()
+    #     dataframe["event_type"] = le.fit_transform(dataframe["event_type"])
+    #     dataframe.metadata = dataframe.metadata.add_semantic_type(
+    #         (metadata_base.ALL_ELEMENTS, 6),
+    #         "httpse://metadata.datadrivendiscovery.org/types/Target",
+    #     )
+    #     dataframe.metadata = dataframe.metadata.add_semantic_type(
+    #         (metadata_base.ALL_ELEMENTS, 6),
+    #         "https://metadata.datadrivendiscovery.org/types/CategoricalData",
+    #     )
+    #     dataframe.metadata = dataframe.metadata.remove_semantic_type(
+    #         (metadata_base.ALL_ELEMENTS, 6),
+    #         "https://metadata.datadrivendiscovery.org/types/Attribute",
+    #     )
+    #     hyperparams_class = ColumnParserPrimitive.metadata.query()["primitive_code"][
+    #         "class_type_arguments"
+    #     ]["Hyperparams"]
+    #     column_parser = ColumnParserPrimitive(hyperparams=hyperparams_class.defaults())
+    #     dataframe = column_parser.produce(inputs=dataframe).value
+
+    #     hyperparams_class = IsolationForestPrimitive.metadata.query()["primitive_code"][
+    #         "class_type_arguments"
+    #     ]["Hyperparams"]
+    #     hyperparams = hyperparams_class.defaults().replace({"n_jobs": -1})
+
+    #     isp = IsolationForestPrimitive(hyperparams=hyperparams)
+    #     isp.set_training_data(
+    #         inputs=dataframe[
+    #             [
+    #                 "iso",
+    #                 "year",
+    #                 "inter1",
+    #                 "inter2",
+    #                 "interaction",
+    #                 "latitude",
+    #                 "longitude",
+    #                 "geo_precision",
+    #                 "fatalities",
+    #             ]
+    #         ],
+    #         outputs=pd.DataFrame({"charlie": dataframe["event_type"].astype(int)}),
+    #     )
+    #     isp.fit()
+    #     results = isp.produce(
+    #         inputs=dataframe[
+    #             [
+    #                 "iso",
+    #                 "year",
+    #                 "inter1",
+    #                 "inter2",
+    #                 "interaction",
+    #                 "latitude",
+    #                 "longitude",
+    #                 "geo_precision",
+    #                 "fatalities",
+    #             ]
+    #         ]
+    #     ).value
+
+    #     self.assertListEqual(
+    #         list(results["label"]), [-1, -1, -1, -1, -1, -1, -1, -1, -1]
+    #     )
+
+
+if __name__ == "__main__":
+    unittest.main()
